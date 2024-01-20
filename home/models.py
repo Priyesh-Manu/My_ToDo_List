6cdc2d3e34e60a816f45561a2ce2dddef49c3095
Priyesh Manu <priyeshmanumk@gmail.com>
2024-01-20 22:35:01 +0530

    First function created. the function use to get and show the data given in the form in our website

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+class MyTask(models.Model):
+    task_name = models.CharField(max_length=100)
+    task_des = models.TextField()
+    task_date = models.DateField(default=datetime.date.today)
+    
+    def __str__(self):
+        return self.task_name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import MyTask
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    tasks={
+        'my_task':MyTask.objects.all()
+    }
+    if request.method=='POST':
+        task_name = request.POST['name']
+        task_des = request.POST['description']
+        task_date = request.POST['date']
+        user = MyTask(task_name=task_name,task_des=task_des,task_date=task_date)
+        user.save()
+        return redirect ('/')
+    return render(request,'index.html',tasks)
